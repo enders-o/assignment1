4d7cb5ba1b4ce52250a0091926bf1e74a0ccd176
Ender's Laptop <ender_sotelo@outlook.com>
2022-11-27 11:22:27 -0800

    Implemented new note button

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 const themeButton = document.querySelector('#dark-theme');
 const cancelButton = document.querySelector("#cancel")
+const newNoteButton = document.querySelector("#new-note")
 // console.log(themeButton);
 
 const darkTheme = () => {
@@ -20,8 +21,19 @@ const cancel = () => {
     document.querySelector('textarea').style.display = 'none';
     document.querySelector('#save').style.display = 'none';
     document.querySelector('#cancel').style.display = 'none';
+}
 
+const newNote = () => {
+    if ( document.querySelector('textarea').style.display === 'none') {
+        document.querySelector('textarea').style.display = 'block';
+        document.querySelector('#save').style.display = 'inline-block';
+        document.querySelector('#cancel').style.display = 'inline-block';
+        document.querySelector('textarea').value ="";
+    } else {
+        document.querySelector('textarea').value ="";
+    }
 }
 
 themeButton.addEventListener("click", darkTheme);
 cancelButton.addEventListener("click", cancel);
+newNoteButton.addEventListener("click", newNote)
